78efd48fdc3dc9ba4b4e01fc3d6a45f4e5c4e0ca
Thomas Frazier <tfrazier2424@gmail.com>
2021-06-28 14:30:01 -0400

    added models from ERD

---
+++ back-end-capstone/Models/ChallengeAttempt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end_capstone.Models
+{
+    public class ChallengeAttempt
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ChallengeId { get; set; }
+        public int Attempts { get; set; }
+
+        [MaxLength(255)]
+        public string Notes { get; set; }
+
+    }
+}
+
---
+++ back-end-capstone/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end_capstone.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+    }
+}
